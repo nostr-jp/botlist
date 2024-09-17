514a63d5566e37c0f52460e8caa12b866d5ed70e
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2024-09-18 01:54:34 +0900

    update

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 cat botlist.txt | while read LINE; do
   if [ ! -e $LINE.json ]; then
     PK=$(nak decode $LINE | jq -r .pubkey)
-    cat /dev/null | nak req -k 0 -a $PK wss://yabu.me 2> /dev/null | jq -r '.content|fromjson' > $LINE.json
+    cat /dev/null | nak req -k 0 -a $PK wss://relay.nostr.band 2> /dev/null | jq -r '.content|fromjson' > $LINE.json
   fi
   DISPLAY_NAME=$(cat $LINE.json | jq -r .display_name)
   NAME=$(cat $LINE.json | jq -r .name)

6dab360bc7e99cf7d9ca42e6ad3ec88b8e440a10
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2025-11-11 10:37:17 +0900

    update

--- update.sh
+++ update.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+set -e
+
 export PATH=$HOME/bin:$HOME/go/bin:$PATH
 cat botlist.txt | while read LINE; do
   if [ ! -e $LINE.json ]; then
     PK=$(nak decode $LINE | jq -r .pubkey)
-    cat /dev/null | nak req -k 0 -a $PK wss://yabu.me wss://relay-jp.nostr.wirednet.jp wss://nos.lol 2> /dev/null | jq -r '.content|fromjson' > $LINE.json
+    cat /dev/null | nak req -k 0 -a $PK wss://yabu.me wss://relay-jp.nostr.wirednet.jp wss://nos.lol wss://relay.nostr.band 2> /dev/null | jq -r '.content|fromjson' > $LINE.json
+    if [ ! -s $LINE.json ]; then
+      jo "pubkey=$LINE" "name=null"
+      continue
+    fi
   fi
   DISPLAY_NAME=$(cat $LINE.json | jq -r .display_name)
   NAME=$(cat $LINE.json | jq -r .name)
